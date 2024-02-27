aaf791d9aeaefed0d67fafca8b0ab35325c50ab3
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-02-27 23:14:43 +0000

    add faq questions

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+const FAQData = [
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+    {
+        question: "What is the best crypto platform for investing?",
+        answer: "Best crypto platform for investing is Binance, according to users"
+    },
+];
+
+const faqsTable = document.querySelector('#table');
+
+faqsTable.map((faq) => {
+
+})
